b34843149331df5e74432fe8274ddd9f192fa54c
Muhammad Awais <m.awais2030@gmail.com>
2024-01-14 09:04:07 +0500

    User Registration (Email based )

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,31 @@
 'use server'
-import { RegisterSchema } from '@/schemas'
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
+
+import { db } from '@/lib/db'
+import { RegisterSchema } from '@/schemas'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = await RegisterSchema.safeParseAsync(values)
   if (!validatedFields.success) {
     return { error: 'Invalid Fields' }
   }
-  return { success: 'email sent.' }
+
+  const { email, name, password } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+  if (existingUser) return { error: 'Email already in use.' }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  // Todo: Send verification token email
+
+  return { success: 'User Created.' }
 }
