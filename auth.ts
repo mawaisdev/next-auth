6a448e96ea429d4c70aa66af60bf5bd05d421887
Muhammad Awais <m.awais2030@gmail.com>
2024-01-15 15:47:10 +0500

    Expended Session to add User role

--- auth.ts
+++ auth.ts
@@ -1,8 +1,11 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 
+import { getUserById } from '@/data/user'
+
 import { db } from '@/lib/db'
 import authConfig from '@/auth.config'
+import { UserRole } from '@prisma/client'
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +13,27 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      console.log({ sessionToken: token })
+      if (session.user && token.sub) {
+        session.user.id = token.sub
+      }
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole
+      }
+      return session
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token
+      token.role = existingUser.role
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+import { UserRole } from '@prisma/client'
+import NextAuth, { type DefaultSession } from 'next-auth'
+import { Jwt } from '@auth/core/jwt'
+
+export type ExtendedUser = DefaultSession['user'] & {
+  role: UserRole
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: ExtendedUser
+  }
+}
