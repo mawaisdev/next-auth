15429274594cb02aec471973259634594207f530
Muhammad Awais <m.awais2030@gmail.com>
2024-01-15 23:25:28 +0500

    Oauth Complete

--- auth.ts
+++ auth.ts
@@ -13,6 +13,14 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      })
+    },
+  },
   callbacks: {
     async session({ session, token }) {
       console.log({ sessionToken: token })
