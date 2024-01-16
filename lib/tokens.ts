d7d9da47b3d24103bd7b03b7de3a23815be4f699
Muhammad Awais <m.awais2030@gmail.com>
2024-01-16 17:00:54 +0500

    Verification token generation

---
+++ lib/tokens.ts
@@ -0,0 +1,22 @@
+import { v4 as uuidV4 } from 'uuid'
+import { db } from '@/lib/db'
+
+import { getVerificationTokenByEmail } from '@/data/verification-token'
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidV4()
+  const expires = new Date(new Date().getTime() + 3600 * 1000)
+
+  const existingToken = await getVerificationTokenByEmail(email)
+  if (existingToken) {
+    await db.verificationToken.delete({ where: { id: existingToken.id } })
+  }
+  const verificationToken = await db.verificationToken.create({
+    data: {
+      token,
+      expires,
+      email,
+    },
+  })
+  return verificationToken
+}
