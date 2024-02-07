0d4410ef922c3394696bbe5a0c749a4c2be53e0a
Muhammad Awais <m.awais2030@gmail.com>
2024-02-07 15:49:04 +0500

    Refactored accounts actions

--- data/accounts.ts
+++ data/accounts.ts
@@ -2,7 +2,7 @@ import { db } from '@/lib/db'
 
 export const getAccountByUserId = async (userId: string) => {
   try {
-    const account = await db.account.findFirst({ where: { id: userId } })
+    const account = await db.account.findFirst({ where: { userId: userId } })
     return account
   } catch (error) {
     return null
